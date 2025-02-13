7e318c7a6f659d3f6fd8c29f5fb878ab142008a2
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2025-02-13 13:12:37 -1000

    mk_all

--- Carmojones.lean
+++ Carmojones.lean
@@ -1,5 +1,3 @@
--- This module serves as the root of the `Carmojones` library.
--- Import modules here that should be built as part of the library.
 import Carmojones.Basic
-import Carmojones.Finset
 import Carmojones.Canonical
+import Carmojones.Finset
